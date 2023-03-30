9157fd8e9b32df7562b1a8329337e00938ff7334
etn-43 <elivelton.developer@gmail.com>
2023-03-30 16:13:39 -0300

    Sequelize configurado

--- api/app.js
+++ api/app.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
+  res.sendFile(__dirname + 'public/index.html');
 })
-const PORT = 3030;
+const PORT = 3031;
 app.listen(PORT, () =>{
     console.log(`server funcionando na porta ${PORT}`);
 });
